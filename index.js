e32ea4ca8a941b20f63a8798c10478bba812401a
MtheusBarbosa <azulmbs.teteu@gmail.com>
2020-10-27 22:17:02 -0300

    Adição da rota mensagem

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', function(req,res){
     res.send("Olá Matheus!")
 })
 
+app.get('/mensagem', function(req,res){
+    res.send("Essa menssagem é automática.")
+})
+
 app.listen(3000, function(){
     console.log("Conexão inicializada")
 })
