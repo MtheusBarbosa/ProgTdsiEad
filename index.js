6c070c9085252c8005dbab4bd95ba31b80e0d8be
MtheusBarbosa <azulmbs.teteu@gmail.com>
2020-10-27 23:44:57 -0300

    usando ejs

--- index.js
+++ index.js
@@ -14,7 +14,16 @@ app.set("view engine","ejs")
 app.use(express.static(path.join(__dirname, "public")))
 
 app.get('/', function(req,res){
-    res.send("Olá Matheus!")
+    res.render('index.ejs',{})
+})
+
+app.get('/usuarios', function(req,res){
+    res.render('usuarios.ejs',{ usuarios:[
+        {nome:'Matheus', email:'afsfe@gmail.com'},
+        {nome:'Zezinho', email:'zezinho@gmail.com'},
+        {nome:'Braulio', email:'braulio@gmail.com'},
+        {nome:'Juju', email:'jujuo@gmail.com'}
+    ] })
 })
 
 app.listen(3000, function(){
